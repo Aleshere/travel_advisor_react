eaba66cec786ace3621495dff58e4463497151f4
Aleshere <alessioburgio85@gmail.com>
2021-09-17 18:53:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
-function App() {
+import { getPlacesData, getWeatherData } from './api/index';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  const [coords, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+
+  const [autocomplete, setAutocomplete] = useState(null);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const filteredPlaces = places.filter(place => place.rating > rating);
+
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+
+      getWeatherData(coords.lat, coords.lng).then(data => setWeatherData(data));
+      getPlacesData(type, bounds.sw, bounds.ne).then(data => {
+        setPlaces(data?.filter(place => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setRating('');
+        setIsLoading(false);
+      });
+    }
+  }, [type, bounds]);
+
+  const onLoad = autoC => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoordinates({ lat, lng });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setChildClicked={setChildClicked}
+            setBounds={setBounds}
+            coords={coords}
+            setCoordinates={setCoordinates}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
-}
+};
 
 export default App;
